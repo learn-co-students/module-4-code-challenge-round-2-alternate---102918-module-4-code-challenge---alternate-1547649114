cf435d91e78a49e372320b98541d6a79c18a54fc
James Lee <jameslee1187@gmail.com>
2019-01-16 11:04:05 -0500

    bad

--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -1,29 +1,32 @@
 import React from 'react'
 
-const EditStudent = () => {
+const EditStudent = (props) => {
 
-  return (
+    return (
     <form
       className="ui form center aligned sixteen wide column"
-      onSubmit={""}
+
     >
       <div className="inline fields">
         <div className="four wide field">
           <input
             id="name"
             type="text"
+
           />
         </div>
         <div className="four wide field">
           <input
             id="class_year"
             type="number"
+
           />
         </div>
         <div className="four wide field">
-          <input
+          <input 
             id="percentage"
             type="number"
+
           />
         </div>
         <button className="ui button" type="submit">
